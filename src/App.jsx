1d852b514366a1a2c1e25840b55c4b8e7e66cbb3
Olagunju Oluwakolade <holuwakoladey@gmail.com>
2022-09-30 19:03:00 +0100

    further ammendments made

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,10 @@ const App = () => {
     <div className="relative flex">
       <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#A4031F]">
+     
         <Searchbar />
 
-        <div className="px-6 h-[calc(100vh-75px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+        <div className="px-6 h-[calc(100vh-82px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/" element={<Discover />} />
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -13,7 +13,7 @@ const handleSubmit = (e) => {
 };
 
   return (
-<form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+<form onSubmit={handleSubmit} autoComplete="off" className="mt-10 p-2 text-gray-400 focus-within:text-gray-600">
 <label htmlFor="search-field" className="sr-only">
   Search all songs
 </label>
